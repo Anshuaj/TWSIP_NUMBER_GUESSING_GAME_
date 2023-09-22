b70bd8a185879a8d264088c0bc82618161152b3a
Anshuaj <145769483+Anshuaj@users.noreply.github.com>
2023-09-22 20:49:32 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,39 +1,3 @@
-// import java.util.Random;
-// import java.util.Scanner;
-
-// public class NumberGuessingGame {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         Random random = new Random();
-
-//         int num_Guess = random.nextInt(11);
-//         int Total_Tries = 0;
-
-//         boolean GuessedCorrectly = false;
-
-//         System.out.println("Welcome to the Number Guessing Game!");
-        
-//         while (!GuessedCorrectly) {
-//             System.out.print("Guess a number between 0-10: ");
-//             int userGuess = scanner.nextInt();
-//             Total_Tries++;
-
-//             if (userGuess < 0 || userGuess > 10) {
-//                 System.out.println("Please guess a number within the valid range.");
-//             } else if (userGuess < num_Guess) {
-//                 System.out.println("Too low! Try again.");
-//             } else if (userGuess > num_Guess) {
-//                 System.out.println("Too high! Try again.");
-//             } else {
-//                 GuessedCorrectly = true;
-//                 System.out.println("Congratulations! You've guessed the correct number.");
-//                 System.out.println("Total No. of Attempts: " + Total_Tries);
-//             }
-            
-//         }
-//     scanner.close();
-//     }
-// }
 import java.util.Random;
 import java.util.Scanner;
 
